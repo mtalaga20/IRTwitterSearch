761062c2943572e7c03f519d322a68fedd1bf9a3
mtalaga20 <michael.talaga@hotmail.com>
2022-03-07 15:37:20 -0600

    Added notes for future steps

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ from email import header
 from icecream import ic
 import datetime as DT
 import pandas as pd
+import numpy as np
 from Scweet.scweet import scrape
 from Scweet.user import get_user_information, get_users_following, get_users_followers
 
@@ -21,6 +22,10 @@ def test():
     #Short period of time for testing
     day_ago = DT.date.today() - DT.timedelta(days = 1)
 
+    '''
+    Data is a pandas dataframe that consists of tweets and
+    and their associated information like username, timestamp, likes, and comments.
+    '''
     data = scrape(
         words = ['bitcoin', 'ethereum'],  # tweets (or other?) containing these terms?
         since = str(day_ago),
@@ -34,12 +39,26 @@ def test():
 	    resume = False,
         filter_replies = False,
         proximity = False,
-        geocode = '38.3452,-0.481006,200km'  # within this geographic region
+        geocode = '38.3452,-0.481006,20km'  # within this geographic region
     )
     
-    #df = pd.DataFrame(data)
-    print(data)
-    #print(df)
+    #Cleaning
+    data['Timestamp'] = pd.to_datetime(data['Timestamp'])
+    print(data.info())
+    '''
+    Next steps:
+    1. Gather unique words from tweets (unsure if posting list is needed)
+    2. Sort unique words
+    3. Create idf values for each word
+    4. Create weights for each document, append to its respective row in data?
+    5. Be able to accept a query and create weights similar to document
+        - Thinking first query will be a category or buzz word, and 2nd query will be a more narrow search.
+        - Example: Category = Cryptocurrency , query = "Is bitcoin going to go up in price in April?"
+        - This allows a smaller set of tweets given by the category.
+    6. Compute cosine similarity between query and tweets  
+    7. Return n tweets
+    '''
+  
 
 
 def main() -> int:

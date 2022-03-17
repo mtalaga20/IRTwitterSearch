b9428e08543ece7cd714ddd5032cc2a9f891ad9e
mtalaga20 <michael.talaga@hotmail.com>
2022-03-16 19:34:38 -0500

    More functions for tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -2,6 +2,10 @@
 
 import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stopwords = stopwords.words('english')
+ps = nltk.PorterStemmer() #Porters Stemmer
 
 def tokenize(df):
     """
@@ -13,3 +17,9 @@ def tokenize(df):
     df["tweet"] = df["tweet"].str.replace("[^a-zA-Z0–9 ]", "")
     #Tokenize
     df["tweet"] = df["tweet"].apply(lambda x: x.lower().split(" "))
+    #Stopwords
+    df["tweet"] = df["tweet"].apply(lambda x: [word for word in x if word not in stopwords])
+    #Stemmer
+    df["tweet"] = df["tweet"].apply(lambda x: [ps.stem(word) for word in x])
+    #Remove empty spots
+    df["tweet"] = df["tweet"].apply(lambda x: [word for word in x if word != " "])

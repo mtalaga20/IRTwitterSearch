973ca3f3eb5e5e5c065874629617bafedc39c960
mtalaga20 <michael.talaga@hotmail.com>
2022-03-17 20:41:34 -0500

    Fixed space tokens

--- tokenizer.py
+++ tokenizer.py
@@ -9,11 +9,13 @@ ps = nltk.PorterStemmer() #Porters Stemmer
 
 def tokenize(df):
     """
-
+    Tokenizer function to take dataframe columns (tweets), and perform tokenizing, stemming,
+    :param df:
     :return:
     """
 
     #Remove punctuation
+    df["original_tweet"] = df["tweet"]
     df["tweet"] = df["tweet"].str.replace("[^a-zA-Z0–9 ]", "")
     #Tokenize
     df["tweet"] = df["tweet"].apply(lambda x: x.lower().split(" "))
@@ -21,5 +23,5 @@ def tokenize(df):
     df["tweet"] = df["tweet"].apply(lambda x: [word for word in x if word not in stopwords])
     #Stemmer
     df["tweet"] = df["tweet"].apply(lambda x: [ps.stem(word) for word in x])
-    #Remove empty spots
-    df["tweet"] = df["tweet"].apply(lambda x: [word for word in x if word != " "])
+    # Remove empty spots
+    df["tweet"] = df["tweet"].apply(lambda x: [word for word in x if word != " " and word != ""])

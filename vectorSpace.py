f2a9ac9a526829b03dfd2bff9adc6c3ce3564c4f
Will Thomas <30wthomas@gmail.com>
2022-03-28 01:01:49 -0500

    Adding query vector space code (untested)

--- vectorSpace.py
+++ vectorSpace.py
@@ -19,4 +19,42 @@ def vectorSpace(invertedIndex, term_count, doc_count):
             vector_space[docID][term_position] = idf * tf
         term_position += 1
     return vector_space
-#def queryVector(query, )
+
+
+def queryVector(query_data_frame, term_idf_dict, term_count):
+    """
+    :param query_data_frame: Data frame for query (post-tokenizing)
+    :param term_idf_dict: Dictionary mapping term-> idf ( "Term": IDF , ...)
+    :param term_count: Integer for number of unique terms
+        # NOTE: Maybe we can replace with len(term_idf_dict)?
+    :return: Vector 1 x (term_count) size representing the query
+    """
+
+    vector = [0 for i in range(term_count)]
+
+    term_pos = 0
+    for word in query_data_frame["tweet"]:
+        if word in term_idf_dict:
+            # Position = old value (in case duplicate words in query) + idf * 1 
+            #               The use of old value makes idf * 1 work
+            vector[term_pos] = vector[term_pos] + term_idf_dict[word]
+        term_pos += 1
+    return vector
+
+def vectorMagnitude(vector):
+    sum = 0
+    for i in range(len(vector)):
+        sum += vector[i]**2
+    return (sum**0.5)
+
+def cosineSimilarity(doc_vector, query_vector):
+    doc_magnitude = vectorMagnitude(doc_vector)
+    query_magnitude = vectorMagnitude(query_vector)
+    numerator = 0
+
+    # Foreach value in vector take query[i] * doc[i]
+    for i in range(len(doc_vector)):
+        # NOTE: Possible optimization here for 0 values?
+        numerator += doc_vector[i] * query_vector[i]
+    
+    return (numerator / (doc_magnitude * query_magnitude))

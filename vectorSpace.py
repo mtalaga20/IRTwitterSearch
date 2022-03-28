cf6b75c916d3e48ce709aa418fa61841194292b1
mtalaga20 <michael.talaga@hotmail.com>
2022-03-28 17:55:48 -0500

    Cleanup

--- vectorSpace.py
+++ vectorSpace.py
@@ -1,25 +1,23 @@
 def vectorSpace(invertedIndex, term_count, doc_count):
     """
-
-    :param invertedIndex:
+    Vector space function to create weights for each term in respect to each document.
+    Weights calculated by the term's idf multiplied by the doc's respective term frequency.
+    :param invertedIndex: Dictionary featuring each term's associated term frequency,
+    doc frequency, inverse-doc frequency,and a list of the postings list
     :param term_count: Integer for the number of unique terms
-    :return:
+    :param doc_count: Integer for the number of tweets
+    :return: 2D list of the vector space for the documents (list of floats)
     """
     # Vector space dimensions: doc_count x term_count
     vector_space = [[0 for i in range(term_count)] for i in range(doc_count)]
-    #print(f"Term {term_count} \nDoc {doc_count}")
-    #print(vector_space)
     term_position = 0
     for term, values in invertedIndex.items():
         idf = values[0][2]
-        #print(term[0], values, len(vector_space), doc_count , len(vector_space[0]))
         for doc in values[1]: #Update positions in vector space
             docID, tf = doc[0], doc[1]
-            #print(docID, tf)
             vector_space[docID][term_position] = idf * tf
         term_position += 1
     return vector_space
-
 
 def createQueryVector(queryList, invertedIndex, term_count):
     """
